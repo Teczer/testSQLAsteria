4051641d250b1128e335d70bb465751ce80ee680
mehdi_hattou <mehdi_hattou@carrefour.com>
2023-06-28 15:08:30 +0200

    test fix sql

--- server/utils/athlete.js
+++ server/utils/athlete.js
@@ -3,7 +3,27 @@ import { pool } from "./sport.js";
 // GET
 
 export const getAthletesSQL = async () => {
-  const [rows] = await pool.query("SELECT * FROM Athlete");
+  const sqlQuery = `
+  SELECT
+  q.question_value AS questionValue,
+  JSON_ARRAYAGG(
+      JSON_OBJECT(
+          'questionAnswer', op.question_answer,
+          'isCorrect', op.is_correct
+      )
+  ) AS questionOptions,
+  q.photo_question AS photoQuestion,
+  q.photo_answer AS photoAnswer,
+  q.answer_explanation AS answerExplanation
+FROM
+  Question q
+  JOIN QuizOption op ON q.id = op.question_id
+GROUP BY
+  q.id
+
+  `;
+
+  const [rows] = await pool.query(sqlQuery);
 
   return rows;
 };
@@ -39,7 +59,6 @@ export const deleteAthleteSQLById = async (id) => {
   await pool.query(`DELETE FROM Athlete WHERE id = ?`, [id]);
 };
 
-
 export const deletTitreyAthelete = async (athlete_id) => {
-  await pool.query("DELETE FROM titres WHERE athlete_id = ?", [athlete_id])
-}
+  await pool.query("DELETE FROM titres WHERE athlete_id = ?", [athlete_id]);
+};
--- server/utils/sport.js
+++ server/utils/sport.js
@@ -1,15 +1,16 @@
 import mysql from "mysql2";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const pool = mysql
   .createPool({
     host: "0.0.0.0",
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASS,
-    database: "simplon_jo",
+    database: "asteriaquizz",
     port: 3306,
+    charset: "utf8mb4",
   })
   .promise();
 
@@ -19,21 +20,20 @@ export const getSportsSQL = async () => {
 };
 
 ///
-export const deleteSportSQL = async (id) =>
-{
+export const deleteSportSQL = async (id) => {
   await pool.query("DELETE FROM Sport WHERE id = ? ", [id]);
 };
 
-
 export const createSportSQL = async (nom, image) => {
-  await pool.query("INSERT INTO Sport (nom, image) VALUES (?, ?)", [nom, image]);
+  await pool.query("INSERT INTO Sport (nom, image) VALUES (?, ?)", [
+    nom,
+    image,
+  ]);
 };
 
-
-export const modifySportSQL = async (nom,id) =>
-{
-    await pool.query("UPDATE Sport SET nom = ? WHERE id = ?",[nom,id])
-}
+export const modifySportSQL = async (nom, id) => {
+  await pool.query("UPDATE Sport SET nom = ? WHERE id = ?", [nom, id]);
+};
 
 export const getSportSQL = async (id) => {
   const [rows] = await pool.query("SELECT * FROM Sport where id = ?", [id]);
